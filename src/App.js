1bfd6795466f3005daa769bdfd6e1b012f8931ec
Denis Toledo <contato@denistoledo.com.br>
2019-12-23 15:43:38 -0300

    adds html and css for the login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
   return (
-    <h1>Hello world</h1>
+    <div className="container">
+      <img src={logo} alt="AirCnC Logo" />
+      <div class="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e econtre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form action="">
+          <label htmlFor="email">E-MAIL *</label>
+          <input
+            type="text"
+            id="email"
+            placeholder="Seu melhor e-mail"
+          />
+          <button type="submit" className="btn">Entrar</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
