e4394aaa2faa1e2f8b0a731442c42971c67ead8a
Denis Toledo <contato@denistoledo.com.br>
2019-12-24 14:27:39 -0300

    adds service api and starting handle submit"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(email);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC Logo" />
-      <div class="content">
+      <div className="content">
         <p>
           Ofereça <strong>spots</strong> para programadores e econtre <strong>talentos</strong> para sua empresa
         </p>
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           <input
             type="text"
             id="email"
             placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
           />
           <button type="submit" className="btn">Entrar</button>
         </form>
